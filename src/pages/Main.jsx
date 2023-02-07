77769d8d33205bb79577db29a4dc6a2afd3902c0
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-02-07 10:54:27 +0900

    banner in main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Products from './Products'
 
 export default function Main() {
   return (
-    <div>
-      
-    </div>
+    <section className='w-5/6 m-auto'>
+      <article className='w-full h-72 bg-banner bg-cover bg-bottom' >
+        <p className='w-1/3 pt-6 text-4xl text-center'>
+          <span className='block p-2'>Shop</span>
+          <span className='block p-2 text-xl'>With</span>
+          <span className='block p-2'>Us</span>
+        </p>
+        <p className='w-1/3 mt-7 text-center'>Best Products, High Quality</p>
+      </article>
+      <Products />
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner': "url('/public/image/cutted-banner2.png')",
+      }
+    },
   },
   plugins: [],
 }
